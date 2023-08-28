d8db00caa6a8c22943546c2035b753165bfaa851
AbdallaFathy1121 <abdallafathy1023@gmail.com>
2023-08-28 23:24:10 +0300

    Update CartComponent and StoreDatawithAPI

--- src/app/carts/components/cart/cart.component.ts
+++ src/app/carts/components/cart/cart.component.ts
@@ -1,32 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CartProduct } from 'src/app/products/models/cartProduct.model';
+import { CartsService } from '../../services/carts.service';
+import { Subscription } from 'rxjs';
+import { CartStorageService } from '../../services/cart-storage.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css'],
 })
-export class CartComponent implements OnInit {
+export class CartComponent implements OnInit, OnDestroy {
   cartProducts: CartProduct[] = [];
   total: number = 0;
+  cartProductsSubscription!: Subscription;
+  totalSubscription!: Subscription;
+  isLoading: boolean = false;
 
-  constructor() {}
+  constructor(
+    private cartService: CartsService,
+    private cartStorageService: CartStorageService
+  ) {}
 
   ngOnInit(): void {
-    this.getCartProducts();
+    this.cartProducts = this.cartService.getCartProducts();
+    this.total = this.cartService.getCartTotal();
+    this.cartProductsSubscription = this.cartService.changedCartProducts.subscribe(
+      (items) => {
+        this.cartProducts = items;
+      });
+    this.totalSubscription = this.cartService.changedTotal.subscribe(
+      (totla) => {
+        this.total = totla;
+      }
+    );
   }
 
-  getCartProducts() {
-    if ('cart' in localStorage) {
-      this.cartProducts = JSON.parse(localStorage.getItem('cart')!);
-      this.getCartTotal();
-    }
+  ngOnDestroy(): void {
+    this.cartProductsSubscription.unsubscribe();
+    this.totalSubscription.unsubscribe();
   }
 
-  getCartTotal() {
-    for (let x in this.cartProducts) {
-      this.total +=
-        this.cartProducts[x].item.price * this.cartProducts[x].quantity;
-    }
+  onChangeQuantity() {
+    this.cartService.changeQuantity(this.cartProducts);
+  }
+
+  onDeleteProduct(index: number) {
+    this.cartService.deleteProduct(index);
+  }
+
+  onClearCart() {
+    this.cartService.clearCart();
+  }
+
+  sendNewCart() {
+    let products = this.cartProducts.map((item) => {
+      return { productId: item.item.id, quantity: item.quantity };
+    });
+
+    let model = {
+      userId: 5,
+      date: new Date(),
+      products: products,
+    };
+
+    this.isLoading = true;
+    this.cartStorageService.sendNewCart(model).subscribe((res) => {
+      this.cartService.clearCart();
+      console.log(res);
+      this.isLoading = false;
+    });
   }
 }
--- src/app/carts/services/carts.service.ts
+++ src/app/carts/services/carts.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CartProduct } from 'src/app/products/models/cartProduct.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartsService {
+  cartProducts: CartProduct[] = [];
+  total: number = 0;
+  changedCartProducts = new Subject<CartProduct[]>();
+  changedTotal = new Subject<number>();
+  
+  getCartProducts() {
+    if ('cart' in localStorage) {
+      return this.cartProducts = JSON.parse(localStorage.getItem('cart')!);
+    }
+  }
 
-  constructor() { }
+  getCartTotal() {
+    this.total = 0;
+    for (let x in this.cartProducts) {
+      this.total +=
+        this.cartProducts[x].item.price * this.cartProducts[x].quantity;
+    }
+    this.changedTotal.next(this.total);
+    return this.total;
+  }
+
+  changeQuantity(cartProducts: CartProduct[]) {
+    this.cartProducts = cartProducts;
+    this.changedCartProducts.next(this.cartProducts.slice());
+    localStorage.setItem('cart', JSON.stringify(this.cartProducts));
+    this.getCartTotal();
+  }
+
+  deleteProduct(index: number) {
+    this.cartProducts.splice(index, 1);
+    this.changedCartProducts.next(this.cartProducts.slice());
+    localStorage.setItem('cart', JSON.stringify(this.cartProducts));
+    this.getCartTotal();
+  }
+
+  clearCart() {
+    this.cartProducts = [];
+    this.changedCartProducts.next(this.cartProducts.slice());
+    localStorage.setItem('cart', JSON.stringify(this.cartProducts));
+    this.getCartTotal();
+  }
+
 }
